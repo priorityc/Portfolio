d49d3889e7428b32e8bde91d81ecd03d0970f569
Petya Dimitrova <petya83dimitrova@gmail.com>
2026-07-24 10:32:40 +0100

    adding faq section

--- service.js
+++ service.js
@@ -1,7 +1,8 @@
 const tabs = document.querySelectorAll(".service-tab");
-console.log(tabs);
 const panels = document.querySelectorAll(".service-panel");
-console.log(panels);
+
+const faqbtn = document.querySelectorAll(".faq-category__title");
+const faqpanels = document.querySelectorAll(".faq-brand");
 
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
@@ -16,6 +17,23 @@ tabs.forEach((tab) => {
 
     // 4. Activate matching panel
     const target = tab.dataset.tab; // "webdev", "uiux", "branding"
+    document.getElementById(target).classList.add("active");
+  });
+});
+
+faqbtn.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    // 1. Remove active from all tabs
+    faqbtn.forEach((t) => t.classList.remove("active"));
+
+    // 2. Remove active from all panels
+    faqpanels.forEach((p) => p.classList.remove("active"));
+
+    // 3. Activate clicked tab
+    faq.classList.add("active");
+
+    // 4. Activate matching panel
+    const target = faq.dataset.faq; // "webdev", "uiux", "branding"
     document.getElementById(target).classList.add("active");
   });
 });
